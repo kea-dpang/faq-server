3d89f71f3f7c0dfcbc8361fcd3289856dce54216
SeungHyeon <namsh1125@naver.com>
2024-02-04 18:56:11 +0900

    Refactor: SecurityConfig 리팩토링

--- src/main/kotlin/kea/dpang/faq/config/SecurityConfig.kt
+++ src/main/kotlin/kea/dpang/faq/config/SecurityConfig.kt
@@ -1,13 +1,7 @@
 package kea.dpang.faq.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import kea.dpang.faq.base.ErrorResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -15,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.*
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
-import java.time.LocalDateTime
 
 
 @Configuration
@@ -45,30 +38,7 @@ class SecurityConfig {
             .headers { headers -> headers.frameOptions { it.disable() } } // X-Frame-Options 비활성화 (IFrame 사용 가능하도록).
             .formLogin { it.disable() } // formLogin 대신 Jwt를 사용하기 때문에 disable로 설정
             .logout { it.disable() } // 로그아웃 기능 비활성화.
-            .exceptionHandling {
-                it.authenticationEntryPoint { request, response, authException ->
-                    // 인증 오류 처리 로직
-                    val errorResponse = ErrorResponse(
-                        timestamp = LocalDateTime.now(),
-                        status = HttpStatus.UNAUTHORIZED.value(),
-                        error = HttpStatus.UNAUTHORIZED.name,
-                        message = authException?.message ?: "세부 정보가 제공되지 않았습니다",
-                        path = request.requestURI
-                    )
-
-                    // JSON 변환 기능 설정
-                    val mapper = ObjectMapper()
-                        .registerModule(JavaTimeModule())
-                        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-
-                    val responseBody = mapper.writeValueAsString(errorResponse)
-
-                    response.contentType = MediaType.APPLICATION_JSON_VALUE
-                    response.status = HttpStatus.UNAUTHORIZED.value()
-                    response.writer.write(responseBody)
-                }
-            } // 인증되지 않은 사용자가 보호된 리소스에 접근하려고 시도하면 401 Unauthorized를 반환하도록 설정
-            .authorizeHttpRequests { it.anyRequest().permitAll() } // 모든 요청이 인증을 필요로 하도록 설정
+            .authorizeHttpRequests { it.anyRequest().authenticated() } // 모든 요청이 인증을 필요로 하도록 설정
             .build()
     }
 }
